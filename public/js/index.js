e1d2ed1d6e1b4cdd322de4166b3b2857f25de5a5
Marcelino Rodrigues <marcelino.rodrigues.d.n@gmail.com>
2020-10-13 19:07:45 -0300

    add index.js

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+//Usando Variaveis Globais minimizando o tamanho do cÃ³digo
+const $ = query => document.getElementById(query)
+const creat = paramer => document.createElement(paramer)
+
+//evento de click para validar o Login do usuÃ¡rio
+$('submit').addEventListener('click', () => {
+    const user = $('user').value
+    const pass = $('pass').value
+
+    //se nÃ£o houver um dos campos um alerta Ã© exibido
+    if( !user || !pass){
+        $('danger').style.display = 'flex'
+    }
+})
+
+// ao clicar no alerta ele irÃ¡ sumir
+$('danger').addEventListener('click', () => {
+    $('danger').style.display = 'none'
+})
